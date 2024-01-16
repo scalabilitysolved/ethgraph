c0d187e4c6ee5b9d145c9322527df3fd300118b9
Owen Convey <owenconvey@gmail.com>
2024-01-16 13:07:22 +0000

    Intitial commit

---
+++ main.ts
@@ -0,0 +1,113 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+// Replace with your Etherscan API Key
+const apiKey: any = process.env.ETHERSCAN_API_KEY;
+
+interface EtherscanApiResponse {
+    status: string;
+    message: string;
+    result: EthereumTransaction[];
+}
+
+interface EthereumTransaction {
+    blockNumber: string;
+    timeStamp: string;
+    hash: string;
+    nonce: string;
+    blockHash: string;
+    transactionIndex: string;
+    from: string;
+    to: string;
+    value: string;
+    gas: string;
+    gasPrice: string;
+    isError: string;
+    txreceipt_status: string;
+    input: string;
+    contractAddress: string;
+    cumulativeGasUsed: string;
+    gasUsed: string;
+    confirmations: string;
+    methodId: string;
+    functionName: string;
+}
+
+export interface EthereumAddress {
+    address: string;
+    senders: EthereumAddress[];
+    depth: number;
+}
+
+async function getAccountTransactions(address: string, apiKey: string): Promise<EthereumTransaction[]> {
+    const url = `https://api.etherscan.io/api`;
+    const params = {
+        module: 'account',
+        action: 'txlist',
+        address: address,
+        startblock: 0,
+        endblock: 99999999,
+        sort: 'asc',
+        apiKey: apiKey
+    };
+
+    try {
+        console.log(`Fetching transactions for address: ${address}`);
+        const response = await axios.get<EtherscanApiResponse>(url, {params});
+        let result = response.data.result;
+        console.log(`Fetched ${result.length} transactions for address: ${address}`);
+        return result.slice(0, 100); // Limiting the number of transactions for simplicity
+    } catch (error) {
+        console.error(`Error fetching transactions: ${error}`);
+        throw (error);
+    }
+}
+
+async function extractAddresses(address: string, transactions: EthereumTransaction[], depth: number, maxDepth: number): Promise<EthereumAddress> {
+    let accountRelationship: EthereumAddress = {
+        address: address,
+        senders: [],
+        depth: depth
+    };
+
+    if (depth >= maxDepth) {
+        return accountRelationship;
+    }
+
+    const transactionsMap = transactions.reduce((acc: any, transaction) => {
+        if (transaction.from === address) {
+            return acc;
+        }
+
+        if (!acc[transaction.from]) {
+            acc[transaction.from] = [];
+        }
+        acc[transaction.from].push(transaction);
+        return acc;
+    }, {});
+
+    console.log(`Address: ${address} has ${Object.keys(transactionsMap).length} senders`);
+    //log the transactions
+    console.log(`Address: ${address} has senders of ${Object.keys(transactionsMap)}`);
+
+    for (const key of Object.keys(transactionsMap)) {
+        if (key !== address) {
+            if (depth < maxDepth - 1) {
+                // Fetch transactions and recursively call extractAddresses
+                const senderTransactions = await getAccountTransactions(key, apiKey);
+                const senderAddress = await extractAddresses(key, senderTransactions, depth + 1, maxDepth);
+                accountRelationship.senders.push(senderAddress);
+            } else {
+                // Just add sender addresses without further transactions
+                accountRelationship.senders.push({address: key, senders: [], depth: depth + 1});
+            }
+        }
+    }
+
+    return accountRelationship;
+}
+
+export async function run(address: string): Promise<EthereumAddress> {
+    let transactions = await getAccountTransactions(address, apiKey);
+    return extractAddresses(address, transactions, 0, 2);
+}
---
+++ server.ts
@@ -0,0 +1,128 @@
+import express from 'express';
+import {run, EthereumAddress} from './main';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/data', async (req, res) => {
+    const address = req.query.address; // Retrieve the address from the query parameter
+    if (!address) {
+        res.status(400).json({error: 'Ethereum address is required'});
+        return;
+    }
+
+
+    try {
+
+        // if address is daddy, return test data
+        if (address.toString() === "daddy") {
+            res.json(testData());
+            return;
+        }
+        const accountRelationship = await run(address.toString());
+        res.json(accountRelationship);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+
+function testData() {
+    let a: EthereumAddress = {
+        address: "daddy",
+        senders: [],
+        depth: 0
+    };
+
+    let babyOneChild: EthereumAddress = {
+        address: "0xbd237a6df1aae2faa85ef690a33768f73d41604f",
+        senders: [],
+        depth: 1
+    };
+
+    let babyTwoChild: EthereumAddress = {
+        address: "babyTwoChild",
+        senders: [],
+        depth: 1
+    }
+
+    let babyThreeChild: EthereumAddress = {
+        address: "babyThreeChild",
+        senders: [],
+        depth: 1
+    }
+
+    let babyNoChildren: EthereumAddress = {
+        address: "babyNoChildren",
+        senders: [],
+        depth: 1
+    };
+
+    let grandBaby: EthereumAddress = {
+        address: "grandBaby",
+        senders: [],
+        depth: 2
+    };
+
+    let grandBabyTwo: EthereumAddress = {
+        address: "grandBabyTwo",
+        senders: [],
+        depth: 2
+    };
+
+    let grandBabyThree: EthereumAddress = {
+        address: "grandBabyThree",
+        senders: [],
+        depth: 2
+    }
+
+    let grandBabyFour: EthereumAddress = {
+        address: "grandBabyFour",
+        senders: [],
+        depth: 2
+    }
+
+    let greatGrandBaby: EthereumAddress = {
+        address: "greatGrandBaby",
+        senders: [],
+        depth: 3
+    }
+
+    let greatGrandBabyTwo: EthereumAddress = {
+        address: "greatGrandBabyTwo",
+        senders: [],
+        depth: 3
+    }
+
+    let greatGrandBabyThree: EthereumAddress = {
+        address: "greatGrandBabyThree",
+        senders: [],
+        depth: 3
+    }
+
+    let greatGrandBabyFour: EthereumAddress = {
+        address: "greatGrandBabyFour",
+        senders: [],
+        depth: 3
+    }
+
+    let greatGreatGrandBaby: EthereumAddress = {
+        address: "greatGreatGrandBaby",
+        senders: [],
+        depth: 4
+    }
+
+
+    a.senders.push(babyOneChild, babyNoChildren, babyTwoChild, babyThreeChild);
+    babyOneChild.senders.push(grandBaby, grandBabyTwo);
+    babyThreeChild.senders.push(grandBabyThree, grandBabyFour);
+    grandBabyFour.senders.push(greatGrandBaby, greatGrandBabyTwo, greatGrandBabyThree, greatGrandBabyFour);
+    greatGrandBabyThree.senders.push(greatGreatGrandBaby);
+    return a;
+}
